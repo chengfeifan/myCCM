2b3cdfe6508eba6fffca8d1693565a6253fc4343
chengfeifan <chengff11@gmail.com>
2016-04-13 20:17:40 +0800

    debug paraCCMRes.R

--- R/paraCCMRes.R
+++ R/paraCCMRes.R
@@ -11,26 +11,25 @@
 #' @return Return a list of time lag sequence, yCausex and xCausey
 #' @export
 
-paraCCMRes<-function(x,y,maxLag=20,timestep=1,tag,E=2,...){
+paraCCMRes<-function(x,y,maxLag=20,timestep=1,tag=c(1:length(x)),E=2,...){
   library(parallel)
   tlag<-seq(-maxLag,maxLag,timestep)
   # y cause x
   cl<-makeCluster(getOption("cl.cores",8))
   clusterExport(cl,"myCCMRes")
-  corY_X<-unlist(parLapply(cl,tlag,function(i){
+  corY_X<-unlist(parLapply(cl,tlag,function(i,x,y){
     corX<-myCCMRes(x,y,tag=tag,lag=i)
     return(corX)
-  }))
+  },x,y))
   corX<-corY_X[seq(2,length(corY_X),2)]
 
   # x cause y
-  corX_Y<-unlist(parLapply(cl,tlag,function(i){
+  corX_Y<-unlist(parLapply(cl,tlag,function(i,x,y){
     corY<-myCCMRes(y,x,tag=tag,lag=i)
     return(corY)
-  }))
+  },x,y))
   corY<-corX_Y[seq(2,length(corX_Y),2)]
   stopCluster(cl)
-
   # plot the result
   opar<-par(no.readonly = TRUE)
   par(lty=2,pch=2,lwd=2)
