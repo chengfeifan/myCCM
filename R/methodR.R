96078dbac653f8db76b89035d01a48ccda720299
chengfeifan <chengff11@gmail.com>
2016-05-09 18:43:58 +0800

    methodR.R

--- R/methodR.R
+++ R/methodR.R
@@ -1,1 +1,11 @@
+rm(list=ls())
 library(stats)
+data("BMData")
+i=7
+u<-BMData[[i]]$u
+y<-BMData[[i]]$y
+ccfvalue<-ccf(u,y,lag.max = 100)$acf[,,1]
+myccfvalue<-CCF(u,y,lag.max = 100)
+plot(myccfvalue,type='l')
+which.max(myccfvalue$values)
+probEstimate(y)
